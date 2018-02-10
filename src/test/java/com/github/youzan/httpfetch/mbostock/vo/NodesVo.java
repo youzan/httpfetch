4ad56f77bcc87e70da92e93221a4214b2355b2b8
daiqiang <569941672@qq.com>
2018-02-10 17:28:04 +0800

    [][fix]optimize configuration init!

---
+++ src/test/java/com/github/youzan/httpfetch/mbostock/vo/NodesVo.java
@@ -0,0 +1,24 @@
+package com.github.youzan.httpfetch.mbostock.vo;
+
+public class NodesVo {
+
+    private String id;
+
+    private Integer group;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getGroup() {
+        return group;
+    }
+
+    public void setGroup(Integer group) {
+        this.group = group;
+    }
+}
